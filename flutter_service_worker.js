6e5f6968363914bb458580d21d36e3a129229732
abdallah yasser <abdallahyasser1277@gmail.com>
2025-09-03 23:12:18 +0300

    status impl

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -27,7 +27,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "20912d28506a127a50df17c279202042
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "804d4c140b09e03be2484b52ec688b49",
+"flutter_bootstrap.js": "aef234a0918bee17d45d09a86292b360",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
