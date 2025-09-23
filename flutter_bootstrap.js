23dbe12ff9295b5bd9bccde8a2ac772ec815d553
abdallah yasser <abdallahyasser1277@gmail.com>
2025-09-24 00:23:27 +0300

    update

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"a8bfdfc394deaed5c57bd45a64ac4294dc976a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3222924769"
+    serviceWorkerVersion: "769005892"
   }
 });
